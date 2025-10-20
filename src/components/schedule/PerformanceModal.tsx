b6f5aa0cbb36afde63ea9c8cfcdd3bdf9b4ad72e
永吉舞 <mai.nagayoshi@gmail.com>
2025-10-20 13:55:05 +0900

    style: PerformanceModal のサイズを md に変更

--- src/components/schedule/PerformanceModal.tsx
+++ src/components/schedule/PerformanceModal.tsx
@@ -331,7 +331,7 @@ export function PerformanceModal({
 
   return (
     <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
-      <DialogContent size="lg" className="h-[90vh] overflow-hidden flex flex-col">
+      <DialogContent size="md" className="h-[90vh] overflow-hidden flex flex-col">
         <DialogHeader>
           <DialogTitle>{modalTitle}</DialogTitle>
           <DialogDescription>
