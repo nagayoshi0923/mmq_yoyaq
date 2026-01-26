fb29bc8b4eeca05d0b723bab45afd09b4eb66af8
nagayoshi0923 <mai.nagayoshi@gmail.com>
2026-01-26 20:51:19 +0900

    style: モーダルに角丸を追加

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -49,7 +49,7 @@ const DialogContent = React.forwardRef<
       <DialogPrimitive.Content
         ref={ref}
         className={cn(
-          "fixed left-[50%] top-[50%] z-50 grid w-full max-w-[95vw] translate-x-[-50%] translate-y-[-50%] gap-2 sm:gap-3 border bg-background p-2 sm:p-4 md:p-6 shadow-2xl duration-200 overflow-y-auto data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] focus:outline-none [&_*]:focus:ring-0 [&_*]:focus:ring-offset-0 [&_*:focus-visible]:ring-0 [&_*:focus-visible]:ring-offset-0",
+          "fixed left-[50%] top-[50%] z-50 grid w-full max-w-[95vw] translate-x-[-50%] translate-y-[-50%] gap-2 sm:gap-3 rounded-lg border bg-background p-2 sm:p-4 md:p-6 shadow-2xl duration-200 overflow-y-auto data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] focus:outline-none [&_*]:focus:ring-0 [&_*]:focus:ring-offset-0 [&_*:focus-visible]:ring-0 [&_*:focus-visible]:ring-offset-0",
           sizeClasses[size],
           className
         )}
